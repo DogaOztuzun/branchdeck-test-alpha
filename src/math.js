2b7babac26ca71f1fec1997304f31517dc3b951f
darq <darq@bot.dev>
2026-04-03 17:32:43 +0300

    feat: implement issue #33

--- src/math.js
+++ src/math.js
@@ -79,4 +79,11 @@ function min(a, b) {
   return a < b ? a : b;
 }
 
-module.exports = { subtract, divide, multiply, power, absoluteValue, max, min };
+function modulo(a, b) {
+  if (b === 0) {
+    throw new Error('Modulo by zero');
+  }
+  return a % b;
+}
+
+module.exports = { subtract, divide, multiply, power, absoluteValue, max, min, modulo };
